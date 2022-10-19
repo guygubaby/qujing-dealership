fbdba4fbdc31d42bc68eb50a7428a97ea91e8441
guygubaby <guygubaby@gmail.com>
2022-10-19 15:38:46 +0800

    chore: add more features

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig, presetIcons } from 'unocss'
+import { defineConfig, presetIcons, presetUno } from 'unocss'
 import { presetWeapp } from 'unocss-preset-weapp'
 import { transformerClass } from 'unocss-preset-weapp/transformer'
+
+const weappPreset = presetWeapp({
+  dark: 'class',
+})
 
 const include = [/\.wxml$/]
 
@@ -11,48 +15,34 @@ export default defineConfig(
       'flex-center': 'flex justify-center items-center',
     },
     presets: [
-      presetWeapp({
-        dark: 'class',
-      }),
+      weappPreset,
+
+      { ...presetUno(), preflights: weappPreset.preflights },
 
       presetIcons({
         prefix: 'i-',
+        scale: 1.2,
         warn: true,
-        autoInstall: false,
       }),
     ],
 
     rules: [
-      [
-        /^([w|h])-(\d+)$/, (match) => {
-          const [c, p, d] = match
-          const unit = p === 'w' ? 'width' : 'height'
-          const val = isNaN(+d) ? d : `${+d * 8}rpx`
-          return `.${c}{${unit}:${val};}`
-        },
-      ],
-      [
-        /^(?:grid)-(row|col)-(.+)$/, (match) => {
-          const [c, p, d] = match
-          const unit = p === 'col' ? 'columns' : 'rows'
-          const val = isNaN(+d) ? d : `repeat(${d}, minmax(0,1fr))`
-          return `.${c}{grid-template-${unit}:${val};}`
-        },
-      ],
-      [
-        /^(?:snap)-(x|y|center|start)$/, (match) => {
-          const [c] = match
-          const dict: Record<string, string> = {
-            'snap-x': 'scroll-snap-type: x mandatory;',
-            'snap-y': 'scroll-snap-type: y mandatory;',
-            'snap-start': 'scroll-snap-align: start;',
-            'snap-center': 'scroll-snap-align: center;',
-            'snap-end': 'scroll-snap-align: end;',
-          }
-          const val = dict[c] || ''
-          return `.${c}{${val}}`
-        },
-      ],
+      // [
+      //   /^([w|h])-(\d+)$/, (match) => {
+      //     const [c, p, d] = match
+      //     const unit = p === 'w' ? 'width' : 'height'
+      //     const val = isNaN(+d) ? d : `${+d * 8}rpx`
+      //     return `.${c}{${unit}:${val};}`
+      //   },
+      // ],
+      // [
+      //   /^(?:grid)-(row|col)-(.+)$/, (match) => {
+      //     const [c, p, d] = match
+      //     const unit = p === 'col' ? 'columns' : 'rows'
+      //     const val = isNaN(+d) ? d : `repeat(${d}, minmax(0,1fr))`
+      //     return `.${c}{grid-template-${unit}:${val};}`
+      //   },
+      // ],
     ],
 
     transformers: [
